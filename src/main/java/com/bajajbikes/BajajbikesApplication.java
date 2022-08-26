c9dd0bb567c50817887f5861ec4f3d1ec7957cce
@skp9721 <skpshravan787@gmail.com>
2022-08-26 06:50:10 +0530

    marketingapp_1

---
+++ src/main/java/com/bajajbikes/BajajbikesApplication.java
@@ -0,0 +1,13 @@
+package com.bajajbikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajbikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajbikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bajajbikes/BajajbikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajajbikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajbikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
